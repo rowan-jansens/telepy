0a6dddb9ba7896dfb496911e11a8dd8b0d4e175b
Rowan Jansens <rowan.jansens@gmail.com>
2022-08-09 17:27:44 -0600

    added file saving

--- Python/gsui_py.py
+++ Python/gsui_py.py
@@ -15,9 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QMainWindow,
-    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
-    QTabWidget, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLabel,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QSpacerItem, QStatusBar, QTabWidget, QWidget)
 
 from pyqtgraph import PlotWidget
 
@@ -35,34 +35,53 @@ class Ui_MainWindow(object):
         self.centralwidget.setStyleSheet(u"QWidget#centralwidget{\n"
 "	background-color: rgb(58, 58, 58);\n"
 "}")
-        self.gridLayoutWidget = QWidget(self.centralwidget)
-        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
-        self.gridLayoutWidget.setGeometry(QRect(160, 230, 581, 331))
-        self.gridLayout_2 = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.Accel = PlotWidget(self.gridLayoutWidget)
-        self.Accel.setObjectName(u"Accel")
+        self.gridLayout_3 = QGridLayout()
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.Orientation = PlotWidget(self.centralwidget)
+        self.Orientation.setObjectName(u"Orientation")
+        self.Orientation.setStyleSheet(u"background-color: rgb(255, 255, 255);")
 
-        self.gridLayout_2.addWidget(self.Accel, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.Orientation, 0, 2, 1, 1)
 
-        self.Gyro = PlotWidget(self.gridLayoutWidget)
+        self.Gyro = PlotWidget(self.centralwidget)
         self.Gyro.setObjectName(u"Gyro")
+        self.Gyro.setStyleSheet(u"background-color: rgb(255, 255, 255);")
 
-        self.gridLayout_2.addWidget(self.Gyro, 1, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.Gyro, 0, 1, 1, 1)
 
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 10, 411, 51))
-        self.label.setStyleSheet(u"font: 26pt \"Calibri\";\n"
-"background-color: rgb(85, 170, 255);\n"
-"color: rgb(255, 255, 255);\n"
-"selection-background-color: rgb(188, 214, 255);\n"
-"border-radius: 10px;")
-        self.label.setAlignment(Qt.AlignCenter)
+        self.Accel = PlotWidget(self.centralwidget)
+        self.Accel.setObjectName(u"Accel")
+        self.Accel.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+
+        self.gridLayout_3.addWidget(self.Accel, 0, 0, 1, 1)
+
+        self.Altitude = PlotWidget(self.centralwidget)
+        self.Altitude.setObjectName(u"Altitude")
+        self.Altitude.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+
+        self.gridLayout_3.addWidget(self.Altitude, 1, 1, 1, 1)
+
+        self.Velocity = PlotWidget(self.centralwidget)
+        self.Velocity.setObjectName(u"Velocity")
+        self.Velocity.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+
+        self.gridLayout_3.addWidget(self.Velocity, 1, 2, 1, 1)
+
+        self.Position = PlotWidget(self.centralwidget)
+        self.Position.setObjectName(u"Position")
+        self.Position.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+
+        self.gridLayout_3.addWidget(self.Position, 1, 0, 1, 1)
+
+
+        self.gridLayout_2.addLayout(self.gridLayout_3, 1, 0, 1, 3)
+
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
         self.connect_button = QPushButton(self.centralwidget)
         self.connect_button.setObjectName(u"connect_button")
-        self.connect_button.setGeometry(QRect(40, 80, 75, 24))
         self.connect_button.setStyleSheet(u"QPushButton {\n"
 "	background-color: rgb(85, 170, 255);\n"
 "	border: none;\n"
@@ -85,9 +104,16 @@ class Ui_MainWindow(object):
 "}\n"
 "\n"
 "")
+
+        self.gridLayout.addWidget(self.connect_button, 0, 0, 1, 1)
+
+        self.serial_port = QComboBox(self.centralwidget)
+        self.serial_port.setObjectName(u"serial_port")
+
+        self.gridLayout.addWidget(self.serial_port, 0, 1, 1, 1)
+
         self.disconnect_button = QPushButton(self.centralwidget)
         self.disconnect_button.setObjectName(u"disconnect_button")
-        self.disconnect_button.setGeometry(QRect(130, 80, 75, 24))
         self.disconnect_button.setStyleSheet(u"QPushButton {\n"
 "	background-color: rgb(255, 85, 0);\n"
 "	border: none;\n"
@@ -110,6 +136,65 @@ class Ui_MainWindow(object):
 "}\n"
 "\n"
 "")
+
+        self.gridLayout.addWidget(self.disconnect_button, 2, 0, 1, 1)
+
+        self.serial_baud = QComboBox(self.centralwidget)
+        self.serial_baud.addItem("")
+        self.serial_baud.addItem("")
+        self.serial_baud.addItem("")
+        self.serial_baud.addItem("")
+        self.serial_baud.addItem("")
+        self.serial_baud.addItem("")
+        self.serial_baud.addItem("")
+        self.serial_baud.addItem("")
+        self.serial_baud.setObjectName(u"serial_baud")
+
+        self.gridLayout.addWidget(self.serial_baud, 2, 1, 1, 1)
+
+        self.resize_button = QPushButton(self.centralwidget)
+        self.resize_button.setObjectName(u"resize_button")
+        self.resize_button.setStyleSheet(u"QPushButton {\n"
+"	background-color: rgb(117, 117, 117);\n"
+"	border: none;\n"
+"	color: rgb(255, 255, 255);\n"
+"	border-radius: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"	background-color: rgb(67, 135, 202)\n"
+"	border: none;\n"
+"	color: rgb(255, 255, 255);\n"
+"	border-radius: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"	background-color: rgb(85, 170, 0)\n"
+"	border: none;\n"
+"	color: rgb(255, 255, 255);\n"
+"	border-radius: 5px;\n"
+"}")
+
+        self.gridLayout.addWidget(self.resize_button, 3, 0, 1, 2)
+
+
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 2, 1, 1)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout_2.addItem(self.horizontalSpacer, 0, 1, 1, 1)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setStyleSheet(u"font: 26pt \"Calibri\";\n"
+"background-color: rgb(85, 170, 255);\n"
+"color: rgb(255, 255, 255);\n"
+"selection-background-color: rgb(188, 214, 255);\n"
+"border-radius: 10px;")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -126,8 +211,18 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Phenoix VI Ground Controll", None))
         self.connect_button.setText(QCoreApplication.translate("MainWindow", u"Connect", None))
-        self.disconnect_button.setText(QCoreApplication.translate("MainWindow", u"Disconnect", None))
+        self.disconnect_button.setText(QCoreApplication.translate("MainWindow", u" Disconnect ", None))
+        self.serial_baud.setItemText(0, QCoreApplication.translate("MainWindow", u"9600", None))
+        self.serial_baud.setItemText(1, QCoreApplication.translate("MainWindow", u"14400", None))
+        self.serial_baud.setItemText(2, QCoreApplication.translate("MainWindow", u"19200", None))
+        self.serial_baud.setItemText(3, QCoreApplication.translate("MainWindow", u"28800", None))
+        self.serial_baud.setItemText(4, QCoreApplication.translate("MainWindow", u"31250", None))
+        self.serial_baud.setItemText(5, QCoreApplication.translate("MainWindow", u"38400", None))
+        self.serial_baud.setItemText(6, QCoreApplication.translate("MainWindow", u"57600", None))
+        self.serial_baud.setItemText(7, QCoreApplication.translate("MainWindow", u"115200", None))
+
+        self.resize_button.setText(QCoreApplication.translate("MainWindow", u"Resize All Plots", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u" Phenoix VI Ground Controll ", None))
     # retranslateUi
 
--- Python/main.py
+++ Python/main.py
@@ -5,6 +5,7 @@ from gsui_py import Ui_MainWindow
 import numpy as np
 import serial
 import time
+import serial.tools.list_ports
 
 
 
@@ -18,6 +19,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        pg.setConfigOptions(antialias=True)
+
         plot_LUT = {"x_accel": ["Accel", "y"],
                  "y_accel": ["Accel", "r"],
                  "z_accel": ["Accel", "m"],
@@ -25,9 +28,21 @@ class MainWindow(QtWidgets.QMainWindow):
                  "y_gyr": ["Gyro", "r"],
                  "z_gyr": ["Gyro", "m"]}
 
+        ports = list(serial.tools.list_ports.comports())
+        for p in ports:
+            self.ui.serial_port.addItem((str(p)[0:4]))
+        self.ui.serial_baud.setCurrentIndex(7)
+        self.num_lines_read = 0
+
         
         self.ui.connect_button.clicked.connect(self.open_serial)
         self.ui.disconnect_button.clicked.connect(self.close_serial)
+        self.ui.resize_button.clicked.connect(self.resize_plots)
+
+        self.ui.Accel.addLegend()
+        self.ui.Accel.setTitle("Acceleraion", color=[85, 170, 255], size="12pt")
+        self.ui.Gyro.addLegend()
+        self.ui.Gyro.setTitle("Angular Rate", color=[85, 170, 255], size="12pt")
 
         for i in range(6):
             data_series = list(plot_LUT.keys())[i]
@@ -35,24 +50,38 @@ class MainWindow(QtWidgets.QMainWindow):
             series_color = plot_LUT[data_series][1]
             
             setattr(self.ui, data_series, getattr(self.ui, plot_widget).plot(name=data_series, pen=series_color, width=4))
+        
 
 
     def update_plot(self, data_series, x, y):
         getattr(self.ui, data_series).setData(x,y)
+        
 
     def open_serial(self):
+        with open('data.txt', 'w') as f:
+            f.write("mm,ax,ay,az,gx,gy,gz,\n")
+
+
+        
+        # print(self.ui.serial_baud.cu)
         self.init_data()
-        self.ser = serial.Serial("COM4", 115200)  # change COM# if necessary
+        self.ser = serial.Serial(self.ui.serial_port.currentText(), int(self.ui.serial_baud.currentText()))  # change COM# if necessary
         time.sleep(2)
         self.ser.flushInput
         # print(self.ser.readline())
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.fetch_serial_data)
-        self.timer.start(10)
+        self.timer.start(50)
     
     def close_serial(self):
         self.ser.close()
         self.timer.timeout.disconnect()
+        
+
+    def resize_plots(self):
+        self.ui.Accel.autoRange()
+        self.ui.Gyro.autoRange()
+
 
 
     def init_data(self):
@@ -64,6 +93,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def fetch_serial_data(self):
         while (self.ser.in_waiting > 0):
             self.line = self.ser.readline()
+            self.num_lines_read += 1
             self.line = np.asarray(self.line.decode().rstrip('\n').split(","), dtype=np.single)
             # print(line)
             self.data_array = np.append(self.data_array, [self.line], axis=0)
@@ -71,6 +101,11 @@ class MainWindow(QtWidgets.QMainWindow):
             self.data_rate = (int(1 / ((np.mean(np.diff(self.data_array[180:, 0]))) / 1000)))
             if self.data_rate == 0:
                 self.data_rate = 1
+            if self.num_lines_read > self.data_buffer:
+                self.add_data_to_file()
+                self.num_lines_read = 0
+        print(self.num_lines_read)
+        
         GC.update_plot("x_accel", self.x / self.data_rate, self.data_array[:,1])
         GC.update_plot("y_accel", self.x / self.data_rate, self.data_array[:,2])
         GC.update_plot("z_accel", self.x / self.data_rate, self.data_array[:,3])
@@ -78,6 +113,11 @@ class MainWindow(QtWidgets.QMainWindow):
         GC.update_plot("y_gyr", self.x / self.data_rate, self.data_array[:,5])
         GC.update_plot("z_gyr", self.x / self.data_rate, self.data_array[:,6])
 
+
+    def add_data_to_file(self):
+        with open('data.txt', 'a') as f:
+            f.writelines(np.array2string(self.data_array, separator=',', edgeitems=300).replace('[','').replace(']',''))
+            f.write('\n')
 
 
 
